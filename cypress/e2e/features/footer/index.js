6ef8b61a6c6f1b2043870ac988fc65f2d462577b
rubiwan <nadakepensar@gmail.com>
2025-06-09 22:34:23 +0200

    TEST(footer/index.js): Step definition de footer.feature, queda pendiente la implementacion de la logica para pasar el test, en el componente 'Footer.jsx'

---
+++ cypress/e2e/features/footer/index.js
@@ -0,0 +1,10 @@
+/// <reference types="Cypress" />
+import { Given, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Given('el usuario navega a la home', () => {
+    cy.visit('/');
+});
+
+Then('debería ver el nombre {string} en el footer', (name) => {
+    cy.get('div').contains(name).should('be.visible');
+});
